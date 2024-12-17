4177f333e0c48367e8aa1562285b1490f159660a
Pratham <pentesterpratham@gmail.com>
2024-12-17 08:54:08 +0530

    chore: add NotFound and Error components; implement useIsMobile hook; update layout and styles and adding sidebar and header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Geist } from 'next/font/google';
 import './globals.css';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/ui/app-sidebar';
+import Header from '@/components/Header';
 
 const geistSans = Geist({
     subsets: ['latin'],
@@ -18,7 +21,12 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <body className={geistSans.className}>{children}</body>
+            <body className={geistSans.className}>
+                <SidebarProvider>
+                    <AppSidebar />
+                    <Header>{children}</Header>
+                </SidebarProvider>
+            </body>
         </html>
     );
 }
